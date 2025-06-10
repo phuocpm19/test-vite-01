c15db8088d64ca2829376f47a49db06e8589cf09
Phuoc PM <phuocpm19@gmail.com>
2025-06-10 11:02:31 +0700

    update 02

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,7 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-       Phuoc test 01
+       Phuoc test 02
       </div>
     </>
   )
